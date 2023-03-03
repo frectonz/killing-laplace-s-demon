e1de6df04f3d64d7196caceb4d725bc07162e2d8
frectonz <fraol0912@gmail.com>
2023-03-03 09:53:17 +0300

    remove firebase

--- src/firebaseConfig.ts
+++
@@ -1,9 +0,0 @@
-export default {
-  apiKey: "AIzaSyBMqcpH9XdZjJqhQaZ3Bo3-Xbqt-JRYZnc",
-  authDomain: "killing-laplace-s-demon.firebaseapp.com",
-  projectId: "killing-laplace-s-demon",
-  storageBucket: "killing-laplace-s-demon.appspot.com",
-  messagingSenderId: "521496860532",
-  appId: "1:521496860532:web:d117dfbf50112d66c5fe21",
-  measurementId: "G-YK1N05GWF2"
-};
